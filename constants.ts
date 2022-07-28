af2f7ccff45152b356c49984238ba6a76f271cf4
RohitKumarGit <59276987+RohitKumarGit@users.noreply.github.com>
2022-07-28 14:59:05 +0530

    done

--- constants.ts
+++ constants.ts
@@ -1,7 +1,7 @@
 export const constants = {
   NFT_SERVER:
     "https://woxpdvfthtmznhwvymeo.supabase.co/storage/v1/object/public/nfts/",
-  BASE_URL: "http://127.0.0.1:3001",
+  BASE_URL: "https://seal-app-d935r.ondigitalocean.app",
   TRANSFER_TABLE_NAME: "NFTtransfers",
   POLYGON_URL_TX: "https://mumbai.polygonscan.com/tx/",
   POLYGON_URL_ADDRESS: "https://mumbai.polygonscan.com/address/",
--- next.config.js
+++ next.config.js
@@ -9,6 +9,9 @@ const nextConfig = {
     CONTRACT_ADDRESS: "0x4F7AdfCBf661a485816d8bca6b987478998F09eF",
     SOULBOUND_CONTRACT_ADDRESS: "0xBC9484A44aa3D4214B980E08EEc008144b6771dc",
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
