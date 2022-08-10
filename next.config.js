959879621a0bd475c0589e12b6b0199c330ff1ab
RohitKumarGit <59276987+RohitKumarGit@users.noreply.github.com>
2022-08-10 14:02:40 +0530

    done

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    MORALIS_APP_ID: "LA2eBVTFrjgDWo5mSCGdvKkcjlgL4VoyyHqoe6dT",
-    MORALIS_APP_URL: "https://juxypvpe5iky.usemoralis.com:2053/server",
+    MORALIS_APP_ID: "cp1iFN4XAfUYUntsaiwS5u9pd7jFJ2KBuVDcG1Xd",
+    MORALIS_APP_URL: "https://k5ubkqtvaxft.usemoralis.com:2053/server",
     DEEWARR_ACCOUNT: "0x7a2B903BD643c2f36C740c054BD7344B45Ccf9A8",
     CONTRACT_ADDRESS: "0x4F7AdfCBf661a485816d8bca6b987478998F09eF",
     SOULBOUND_CONTRACT_ADDRESS: "0xBC9484A44aa3D4214B980E08EEc008144b6771dc",
